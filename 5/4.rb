d8eede47c72084cf7f6d3bb667596424f96a5496
Matsuzaki Sota <98798507+matsuzaki40326@users.noreply.github.com>
2022-05-01 21:33:14 +0900

    ruby 5-4

--- 5/4.rb
+++ 5/4.rb
@@ -1,7 +1,17 @@
-class Menu
-  attr_accessor :name
-  attr_accessor :price
-end
+require "./food"
+require "./drink"
 
-# Menuクラスのインスタンスを生成して変数menu1に代入してください
-menu1 = Menu.new
+food1 = Food.new(name: "ピザ", price: 800)
+drink1 = Drink.new(name: "コーラ", price: 300)
+
+# food1.calorieに「700」を代入してください
+food1.calorie = 700
+
+# food1.calorieを出力してください
+puts food1.calorie
+
+# drink1.amountに「500」を代入してください
+drink1.amount = 500
+
+# drink1.amountを出力してください
+puts drink1.amount
