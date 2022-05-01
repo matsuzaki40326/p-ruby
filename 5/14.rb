5dde0e42bca37f2451433ff7b95d6a4b1e33b1f0
Matsuzaki Sota <98798507+matsuzaki40326@users.noreply.github.com>
2022-05-01 22:13:02 +0900

    ruby 5-14

--- 5/14.rb
+++ 5/14.rb
@@ -1,14 +1,27 @@
-require "./menu"
+require "./food"
+require "./drink"
 
-menu1 = Menu.new(name: "ピザ", price: 800)
-menu2 = Menu.new(name: "すし", price: 1000)
-menu3 = Menu.new(name: "コーラ", price: 300)
-menu4 = Menu.new(name: "お茶", price: 200)
+food1 = Food.new(name: "ピザ", price: 800, calorie: 700)
+food2 = Food.new(name: "すし", price: 1000, calorie: 600)
+drink1 = Drink.new(name: "コーラ", price: 300, amount: 500)
+drink2 = Drink.new(name: "お茶", price: 200, amount: 400)
 
-# 変数menusを定義して配列を代入してください
-menus = [menu1,menu2,menu3,menu4]
+menus = [food1, food2, drink1, drink2]
 
-# menusに対して繰り返し処理を実行してください
+index = 0
 menus.each do |menu|
-  puts menu.info
+  puts "#{index}. #{menu.info}"
+  index += 1
 end
+
+puts "--------------"
+puts "メニューの番号を選択してください"
+order = gets.chomp.to_i
+
+selected_menu = menus[order]
+puts "選択されたメニュー: #{selected_menu.name}"
+
+puts "個数を入力してください(3つ以上で100円割引)"
+count = gets.chomp.to_i
+
+puts "お会計は#{selected_menu.get_total_price(count)}円です"
